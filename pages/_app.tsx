b95844bdab99e95f05670930d5257b87bb3878b9
Dylan Mooers <dylmooers@gmail.com>
2022-06-04 18:14:05 -0700

    chore: refactor theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,50 @@ import { useState } from 'react';
 import { AppProps } from 'next/app';
 import { getCookie, setCookies } from 'cookies-next';
 import Head from 'next/head';
-import { MantineProvider, ColorScheme, ColorSchemeProvider } from '@mantine/core';
+import {
+  MantineProvider,
+  ColorScheme,
+  ColorSchemeProvider,
+  MantineThemeOverride,
+} from '@mantine/core';
 import { NotificationsProvider } from '@mantine/notifications';
-import { genTheme } from './_theme';
 import { Header } from '../components/Header/Header';
+
+const genTheme = (colorScheme: ColorScheme): MantineThemeOverride => ({
+  colorScheme,
+  colors: {
+    purple: [
+      '#f2ebff',
+      '#dac4ff',
+      '#c19dff',
+      '#a876ff',
+      '#8f4eff',
+      '#7f36ff',
+      '#7627ff',
+      '#5d00ff',
+      '#4e00d8',
+      '#4000b1',
+    ],
+    'light-green': [
+      '#edf8f2',
+      '#c8ead8',
+      '#c8ead8',
+      '#c8ead8',
+      '#5abf89',
+      '#40a56f',
+      '#328157',
+      '#328157',
+      '#153725',
+      '#07120c',
+    ],
+    'background-transparent': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(
+      (n) => `hsla(0, 0%, 0%, 0.${n})`
+    ) as [string, string, string, string, string, string, string, string, string, string],
+  },
+  primaryColor: 'purple',
+  primaryShade: 5,
+  fontFamily: 'Avenir Next',
+});
 
 export default function App(props: AppProps & { colorScheme: ColorScheme }) {
   const { Component, pageProps } = props;
